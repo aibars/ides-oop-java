b5ce6607a325ca48bb579e00714044849d10ce38
Agustín Ibars <agustin.ibars@gmail.com>
2020-08-26 21:12:36 -0300

    change main

--- TP3/Main.java
+++ TP3/Main.java
@@ -1,3 +1,5 @@
+import data.InquilinoRepository;
+import data.UnidadRepository;
 import propiedades.*;
 
 public class Main {
@@ -5,20 +7,21 @@ public class Main {
 
         try {
             Inquilino inq = new Inquilino(35044955, 'M');
+
+
+
+            Inquilino inq2 = new Inquilino(33333333, 'S');
+            Inquilino inq3 = new Inquilino(38386907, 'F');
+
+            var iRepository = new InquilinoRepository();
+
+            Departamento depto = new Departamento("Juan Manuel de Rosas 1857 4º C",inq2, 121.5);
+            Local local = new Local("San Martin 1523 Local 41",inq3 , 20.5);
             Cochera cochera = new Cochera("Paraguay 2400", inq, 10);
 
-            System.out.println(cochera.toString());
-            Inquilino inq2 = new Inquilino(33333333, 'S');
-            Departamento depto = new Departamento("Juan Manuel de Rosas 1857 4º C",inq2, 121.5);
-            System.out.println(depto.toString());
-            Inquilino inq3 = new Inquilino(38386907, 'F');
-            Local local = new Local("San Martin 1523 Local 41",inq3 , 20.5);
-            System.out.println(local.toString());
+            var uRepository = new UnidadRepository();
 
-            System.out.println("\nContratos alquiler:\n");
-            System.out.println(depto.generarContrato());
-            System.out.println(cochera.generarContrato());
-            System.out.println(local.generarContrato("habilitación nroº 123"));
+            iRepository.addInquilino(inq);
 
         } catch (Exception err) {
             System.out.print(err);
